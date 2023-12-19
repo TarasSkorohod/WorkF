3b731d1f3b6bda31b3d796a5d53402c58ae7e514
mt_tarik <skorohodit12@gmail.com>
2023-12-19 19:28:43 +0200

    Test

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,13 +1,8 @@
 "use client";
 import React from "react";
-import ChartOne from "../Charts/ChartOne";
-import ChartThree from "../Charts/ChartThree";
-import ChartTwo from "../Charts/ChartTwo";
-import ChatCard from "../Chat/ChatCard";
+
 import TableOne from "../Tables/TableOne";
-import CardDataStats from "../CardDataStats";
-import TableTwo from "@/components/Tables/TableTwo";
-import TableThree from "@/components/Tables/TableThree";
+
 
 const Dashboard: React.FC = () => {
   return (
